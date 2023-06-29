707e3e8ad7bc89f5861a234974e57857f4e4914c
Pirogova Maria <mashastrelnikova168@gmail.com>
2023-06-29 12:35:11 +0300

    Выполнена четвертая ЛР

--- Занятие 4. Тестирование/Лабораторные задания/task1/main_test.py
+++ Занятие 4. Тестирование/Лабораторные задания/task1/main_test.py
@@ -1,10 +1,17 @@
 import unittest
-from main import *  # импортируем то, что будем тестировать
+from main import Rectangle  # импортируем то, что будем тестировать
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class RectangleTestCase(unittest.TestCase):
+    def test_rectangle_area(self):
+        rectangle = Rectangle("yellow", 6, 9)
+        self.assertEqual(rectangle.rectangle_area(), 54)  # add assertion here
+
+    def test_create_square(self):
+        square = Rectangle.create_square(5)
+        self.assertEqual(square.width, 5)
+        self.assertEqual(square.height, 5)
+        self.assertEqual(square.is_square(square.width, square.height))
 
 
 if __name__ == '__main__':
