a5a51790db997e9b470ec879db2474a308bddee9
Pirogova Maria <mashastrelnikova168@gmail.com>
2023-06-29 11:58:05 +0300

    Выполнена третья ЛР

--- Занятие 3. Инкапсуляция, наследование, полиморфизм/2. Лабораторные задания/task1/main.py
+++ Занятие 3. Инкапсуляция, наследование, полиморфизм/2. Лабораторные задания/task1/main.py
@@ -1,8 +1,16 @@
 class Book:
     """ Базовый класс книги. """
     def __init__(self, name: str, author: str):
-        self.name = name
-        self.author = author
+        self._name = name
+        self._author = author
+
+    @property
+    def name(self):
+        return self.name
+
+    @property
+    def author(self):
+        return self.author
 
     def __str__(self):
         return f"Книга {self.name}. Автор {self.author}"
@@ -11,21 +19,41 @@ class Book:
         return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
 
 
-class PaperBook:
+class PaperBook(Book):
     def __init__(self, name: str, author: str, pages: int):
-        self.name = name
-        self.author = author
-        self.pages = pages
+        super().__init__(name, author)
+        self._pages = pages
+
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, value):
+        if isinstance(value, int) and value > 0:
+            self._pages = value
+        else:
+            raise ValueError("Кол-во страниц должно быть целым, положительным числом")
 
     def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+        return f"Бумажная {super().__str__()}"
 
 
-class AudioBook:
+class AudioBook(Book):
     def __init__(self, name: str, author: str, duration: float):
-        self.name = name
-        self.author = author
-        self.duration = duration
+        super().__init__(name, author)
+        self._duration = duration
+
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def duration(self, value):
+        if isinstance(value, float) and value > 0:
+            self._duration = value
+        else:
+            raise ValueError("Продолжительность должна быть положительным числом с плавающ. запятой")
 
     def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+        return f"Аудио {super().__str__()}"
